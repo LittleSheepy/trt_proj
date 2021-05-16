8d20d5a9c99f96b520d95bcce9e176deb56d0187
LittleSheepy <38182200+LittleSheepy@users.noreply.github.com>
2021-05-16 22:46:41 +0800

    yolov5 封装
    1

--- main.cpp
+++ main.cpp
@@ -4,156 +4,134 @@
 #include <opencv2/opencv.hpp>
 //#include "logging.h"
 #include "common.h"
-#include "yolov5net.h"
 #include "utils.h"
 #include "cuda_utils.h"
+#include "yolov5.h"
+#include "yolov5cfg.h"
+
 using namespace nvinfer1;
 using namespace std;
-int yolov5(int argc, char** argv) {
-    cudaSetDevice(DEVICE);
+using namespace ObjDet;
+using namespace yolov5;
 
-    std::string wts_name = "";
-    std::string engine_name = "";
-    bool is_p6 = false;
-    float gd = 0.0f, gw = 0.0f;
-    std::string img_dir;
-    if (!parse_args(argc, argv, wts_name, engine_name, is_p6, gd, gw, img_dir)) {
-        std::cerr << "arguments not right!" << std::endl;
-        std::cerr << "./yolov5 -s [.wts] [.engine] [s/m/l/x/s6/m6/l6/x6 or c/c6 gd gw]  // serialize model to plan file" << std::endl;
-        std::cerr << "./yolov5 -d [.engine] ../samples  // deserialize plan file and run inference" << std::endl;
-        return -1;
-    }
+//static nvinfer1::PluginRegistrar<YoloPluginCreator> pluginRegistrarYoloPluginCreator{};
+bool parse_args(int argc, char** argv, std::string& wts, std::string& engine, bool& is_p6, float& gd, float& gw, std::string& img_dir) {
+	//if (argc < 4) return false;
+	if (std::string(argv[1]) == "-s" && (argc == 5 || argc == 7)) {
+		wts = std::string(argv[2]);
+		engine = std::string(argv[3]);
+		auto net = std::string(argv[4]);
+		if (net[0] == 's') {
+			gd = 0.33;
+			gw = 0.50;
+		}
+		else if (net[0] == 'm') {
+			gd = 0.67;
+			gw = 0.75;
+		}
+		else if (net[0] == 'l') {
+			gd = 1.0;
+			gw = 1.0;
+		}
+		else if (net[0] == 'x') {
+			gd = 1.33;
+			gw = 1.25;
+		}
+		else if (net[0] == 'c' && argc == 7) {
+			gd = atof(argv[5]);
+			gw = atof(argv[6]);
+		}
+		else {
+			return false;
+		}
+		if (net.size() == 2 && net[1] == '6') {
+			is_p6 = true;
+		}
+	}
+	else if (std::string(argv[1]) == "-d" && argc >= 3) {
+		engine = std::string(argv[2]);
+		if (argc > 3) {
+			img_dir = std::string(argv[3]);
+		}
+	}
+	else {
+		return false;
+	}
+	return true;
+}
 
-    // create a model using the API directly and serialize it to a stream
-    if (!wts_name.empty()) {
-        IHostMemory* modelStream{ nullptr };
-        APIToModel(BATCH_SIZE, &modelStream, is_p6, gd, gw, wts_name);
-        assert(modelStream != nullptr);
-        std::ofstream p(engine_name, std::ios::binary);
-        if (!p) {
-            std::cerr << "could not open plan output file" << std::endl;
-            return -1;
-        }
-        p.write(reinterpret_cast<const char*>(modelStream->data()), modelStream->size());
-        modelStream->destroy();
-        return 0;
-    }
+int yolov5_1(int argc, char** argv) {
+	std::string wts_name = "";
+	std::string engine_name = "";
+	bool is_p6 = false;
+	float gd = 0.0f, gw = 0.0f;
+	std::string img_dir = "";
+	if (!parse_args(argc, argv, wts_name, engine_name, is_p6, gd, gw, img_dir)) {
+		std::cerr << "arguments not right!" << std::endl;
+		std::cerr << "./yolov5 -s [.wts] [.engine] [s/m/l/x/s6/m6/l6/x6 or c/c6 gd gw]  // serialize model to plan file" << std::endl;
+		std::cerr << "./yolov5 -d [.engine] ../samples  // deserialize plan file and run inference" << std::endl;
+		return -1;
+	}
+	YOLOV5 * yolov5 = new YOLOV5();
+	yolov5->Init(engine_name);
+	if (!wts_name.empty()) {
+		yolov5->serialize(wts_name, engine_name, is_p6, gd, gw);
+		return 0;
+	}
+	yolov5->LoadEngine();
+	if (!img_dir.empty()) {
+		std::vector<std::string> file_names;
+		if (read_files_in_dir(img_dir.c_str(), file_names) < 0) {
+			std::cerr << "read_files_in_dir failed." << std::endl;
+			return -1;
+		}
+		for (int f = 0; f < (int)file_names.size(); f++) {
 
-    // deserialize the .engine and run inference
-    std::ifstream file(engine_name, std::ios::binary);
-    if (!file.good()) {
-        std::cerr << "read " << engine_name << " error!" << std::endl;
-        return -1;
-    }
-    char *trtModelStream = nullptr;
-    size_t size = 0;
-    file.seekg(0, file.end);
-    size = file.tellg();
-    file.seekg(0, file.beg);
-    trtModelStream = new char[size];
-    assert(trtModelStream);
-    file.read(trtModelStream, size);
-    file.close();
+			cv::Mat img = cv::imread(img_dir + "/" + file_names[f]);
+			if (img.empty()) continue;
+			auto res = yolov5->predict(img);
+			for (size_t j = 0; j < res.size(); j++) {
+				cv::Rect r = get_rect(img, res[j].bbox);
+				cv::rectangle(img, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
+				cv::putText(img, std::to_string((int)res[j].class_id), cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+			}
+			cv::imwrite("_" + file_names[f], img);
+		}
+	}
+	else {
+		cv::VideoCapture capture;
+		bool res = capture.open(0);
+		while (true) {
+			cv::Mat img;
+			bool red_res = capture.read(img);
+			auto res = yolov5->predict(img);
+			cout << img.cols << "," << img.rows << endl;
 
-    std::vector<std::string> file_names;
-    if (read_files_in_dir(img_dir.c_str(), file_names) < 0) {
-        std::cerr << "read_files_in_dir failed." << std::endl;
-        return -1;
-    }
+			cv::Mat img_lab(img.rows, img.cols + 200, CV_8UC3);
+			//auto& res = batch_res[0];
+			std::cout << "size:" << res.size() << std::endl;
+			std::string str = "";
+			std::string label_list[] = { "0","1","2", "3","4", "5","6", "7","8", "9",
+								 "A", "b","C", "d","E", "F","J","h","P","t",
+								 "L", "U", "u","o", "_" };
+			for (size_t j = 0; j < res.size(); j++) {
+				cv::Rect r = get_rect(img, res[j].bbox);
+				cv::rectangle(img, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
+				cv::putText(img, std::to_string((int)res[j].class_id), cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+				//cv::putText(img, std::string(label_list[(int)res[j].class_id]), cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+				//str.append(std::string(label_list[(int)res[j].class_id]));
+			}
+			img.copyTo(img_lab(cv::Rect(0, 0, img.cols, img.rows)));
 
-    // prepare input data ---------------------------
-    static float data[BATCH_SIZE * 3 * INPUT_H * INPUT_W];
-    //for (int i = 0; i < 3 * INPUT_H * INPUT_W; i++)
-    //    data[i] = 1.0;
-    static float prob[BATCH_SIZE * OUTPUT_SIZE];
-    IRuntime* runtime = createInferRuntime(gLogger);
-    assert(runtime != nullptr);
-    ICudaEngine* engine = runtime->deserializeCudaEngine(trtModelStream, size);
-    assert(engine != nullptr);
-    IExecutionContext* context = engine->createExecutionContext();
-    assert(context != nullptr);
-    delete[] trtModelStream;
-    assert(engine->getNbBindings() == 2);
-    void* buffers[2];
-    // In order to bind the buffers, we need to know the names of the input and output tensors.
-    // Note that indices are guaranteed to be less than IEngine::getNbBindings()
-    const int inputIndex = engine->getBindingIndex(INPUT_BLOB_NAME);
-    const int outputIndex = engine->getBindingIndex(OUTPUT_BLOB_NAME);
-    assert(inputIndex == 0);
-    assert(outputIndex == 1);
-    // Create GPU buffers on device
-    CUDA_CHECK(cudaMalloc(&buffers[inputIndex], BATCH_SIZE * 3 * INPUT_H * INPUT_W * sizeof(float)));
-    CUDA_CHECK(cudaMalloc(&buffers[outputIndex], BATCH_SIZE * OUTPUT_SIZE * sizeof(float)));
-    // Create stream
-    cudaStream_t stream;
-    CUDA_CHECK(cudaStreamCreate(&stream));
+			cv::putText(img_lab, str, cv::Point(650, 200), cv::FONT_HERSHEY_PLAIN, 3, cv::Scalar(0xFF, 0xFF, 0xFF), 4);
+			std::cout << "str:" << str << std::endl;
+			cv::imshow("ÉãÏñÍ· ", img_lab);
+			cv::waitKey(30);
+		}
+	}
 
-    int fcount = 0;
-    for (int f = 0; f < (int)file_names.size(); f++) {
-        fcount++;
-        if (fcount < BATCH_SIZE && f + 1 != (int)file_names.size()) continue;
-        for (int b = 0; b < fcount; b++) {
-            cv::Mat img = cv::imread(img_dir + "/" + file_names[f - fcount + 1 + b]);
-            if (img.empty()) continue;
-            cv::Mat pr_img = preprocess_img(img, INPUT_W, INPUT_H); // letterbox BGR to RGB
-            int i = 0;
-            for (int row = 0; row < INPUT_H; ++row) {
-                uchar* uc_pixel = pr_img.data + row * pr_img.step;
-                for (int col = 0; col < INPUT_W; ++col) {
-                    data[b * 3 * INPUT_H * INPUT_W + i] = (float)uc_pixel[2] / 255.0;
-                    data[b * 3 * INPUT_H * INPUT_W + i + INPUT_H * INPUT_W] = (float)uc_pixel[1] / 255.0;
-                    data[b * 3 * INPUT_H * INPUT_W + i + 2 * INPUT_H * INPUT_W] = (float)uc_pixel[0] / 255.0;
-                    uc_pixel += 3;
-                    ++i;
-                }
-            }
-        }
-
-        // Run inference
-        auto start = std::chrono::system_clock::now();
-        doInference(*context, stream, buffers, data, prob, BATCH_SIZE);
-        auto end = std::chrono::system_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
-        std::vector<std::vector<Yolo::Detection>> batch_res(fcount);
-        for (int b = 0; b < fcount; b++) {
-            auto& res = batch_res[b];
-            nms(res, &prob[b * OUTPUT_SIZE], CONF_THRESH, NMS_THRESH);
-        }
-        for (int b = 0; b < fcount; b++) {
-            auto& res = batch_res[b];
-            //std::cout << res.size() << std::endl;
-            cv::Mat img = cv::imread(img_dir + "/" + file_names[f - fcount + 1 + b]);
-            for (size_t j = 0; j < res.size(); j++) {
-                cv::Rect r = get_rect(img, res[j].bbox);
-                cv::rectangle(img, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
-                cv::putText(img, std::to_string((int)res[j].class_id), cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
-            }
-            cv::imwrite("_" + file_names[f - fcount + 1 + b], img);
-        }
-        fcount = 0;
-    }
-
-    // Release stream and buffers
-    cudaStreamDestroy(stream);
-    CUDA_CHECK(cudaFree(buffers[inputIndex]));
-    CUDA_CHECK(cudaFree(buffers[outputIndex]));
-    // Destroy the engine
-    context->destroy();
-    engine->destroy();
-    runtime->destroy();
-
-    // Print histogram of the output distribution
-    //std::cout << "\nOutput:\n\n";
-    //for (unsigned int i = 0; i < OUTPUT_SIZE; i++)
-    //{
-    //    std::cout << prob[i] << ", ";
-    //    if (i % 10 == 0) std::cout << std::endl;
-    //}
-    //std::cout << std::endl;
-
-    return 0;
 }
 
 int main(int argc, char** argv) {
-
+	yolov5_1(argc, argv);
 }
---
+++ yolov5/include/yolov5.h
@@ -0,0 +1,50 @@
+#ifndef YOLOV5_H
+#define YOLOV5_H
+//#include "common.hpp"
+#include "NvInferRuntime.h"
+#include "NvInfer.h"
+#include "logging.h"
+
+#include <fstream>
+#include <map>
+#include <sstream>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <dirent.h>
+#include "NvInfer.h"
+//#include "yololayer.h"
+#include "yolov5cfg.h"
+using namespace nvinfer1;
+using namespace yolov5;
+namespace ObjDet {
+	class YOLOV5
+	{
+	public:
+		YOLOV5();
+		~YOLOV5();
+		void Init(std::string& engine_name);
+		ICudaEngine* YOLOV5::build_engine(unsigned int maxBatchSize, IBuilder* builder, IBuilderConfig* config, DataType dt, float& gd, float& gw, std::string& wts_name);
+		ICudaEngine* YOLOV5::build_engine_p6(unsigned int maxBatchSize, IBuilder* builder, IBuilderConfig* config, DataType dt, float& gd, float& gw, std::string& wts_name);
+		int YOLOV5::LoadEngine();
+		void YOLOV5::APIToModel(unsigned int maxBatchSize, IHostMemory** modelStream, bool& is_p6, float& gd, float& gw, std::string& wts_name);
+		void doInference();
+		int serialize(std::string& wts_name, std::string& engine_name, bool& is_p6, float& gd, float& gw);
+		//void deserialize();
+		std::vector<yolov5::Detection> predict(cv::Mat& img);
+        std::vector<std::string> predict_str(cv::Mat& img);
+	public:
+		IRuntime*			m_runtime	= nullptr;					// 
+		ICudaEngine*		m_engine	= nullptr;					// 
+		IExecutionContext*	m_context	= nullptr;					// 
+		cudaStream_t		m_stream	= nullptr;					// 
+		std::string			m_engine_name = "";
+        void*				buffers[2];								//
+
+		float				input[BATCH_SIZE * 3 * INPUT_H * INPUT_W];		// 图片数据
+		float				output[BATCH_SIZE * OUTPUT_SIZE];				// 输出数据
+		int					inputIndex;
+		int					outputIndex;
+	};
+}
+#endif // YOLOV5_H
+
